1cb562cd958b99e6e88ba010132556f1e55057f2
Skylinek <Cocacola331@gmail.com>
2018-11-28 08:15:46 +0100

    no comment

--- 08-panda/student.py
+++ 08-panda/student.py
@@ -59,7 +59,7 @@ def load(filename,id):
     resultToJson['total'] =  panda_file.sum()
     resultToJson['passed'] = int(panda_file[panda_file>0].count())
     resultToJson['regression slope']=resultOfRegresion
-    if points16 is not None:
+    if resultOfRegresion != 0:
         resultToJson['date 16'] = datetime.fromordinal(int(points16)).strftime("%Y-%m-%d")
         resultToJson['date 20'] = datetime.fromordinal(int(points20)).strftime("%Y-%m-%d")
 
